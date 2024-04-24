1d5c55b9d8b3c189af0c1fbacd8e9f701e098b23
JhoanSLC <JhoanSLC4@gmail.com>
2024-04-24 17:57:10 -0500

    safe

--- js/calculator.js
+++ js/calculator.js
@@ -1,3 +1,41 @@
-const buttons = document.querySelectorAll('button');
+const BUTTONS = document.querySelectorAll("button");
+const DISPLAY = document.querySelector(".display");
 
-let 
+
+BUTTONS.forEach(button => {
+    button.addEventListener("click", () => {
+        const pressedButton = button.textContent;
+
+        if (DISPLAY.textContent === "") {
+            DISPLAY.textContent = "0";
+        }
+
+        if (button.id === "c") {
+            DISPLAY.textContent = "0";
+            return;
+        }
+
+        if (button.id === "delete") {
+            if (DISPLAY.textContent.length === 1) {
+                DISPLAY.textContent = "0";
+            } else {
+                DISPLAY.textContent = DISPLAY.textContent.slice(0, -1);
+            }
+            return;
+        }
+
+        if (button.id === "=") {
+            DISPLAY.textContent = eval(DISPLAY.textContent);
+            return;
+        }
+
+       
+
+        if (DISPLAY.textContent === "0") {
+            DISPLAY.textContent = pressedButton;
+        } else {
+            DISPLAY.textContent += pressedButton;
+        }
+    })
+
+})
